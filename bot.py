900a16fd7c1811440f6952912e09d227bbb8761c
fear123lox22890 <fffdidididido@gmail.com>
2026-03-24 21:50:09 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,18 @@
 import asyncio
-import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
 
-BOT_TOKEN = os.getenv("7925155197:AAHsae10cg2J7It0DVv--tmdFN8eeptEMm0")
-
-if not BOT_TOKEN:
-    print("❌ Токен не найден!")
-    exit(1)
+BOT_TOKEN = "7925155197:AAHsae10cg2J7It0DVv--tmdFN8eeptEMm0"
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message: types.Message):
-    await message.answer("✅ Бот работает!")
+    await message.answer("✅ Бот работает на хостинге!")
 
 async def main():
-    print("🚀 Запуск...")
+    print("🚀 Бот запущен!")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
